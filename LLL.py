fdae11be8fb0976cb5eaacc48e3707cdff49a54c
shaooly <shaooly.theking@gmail.com>
2021-12-17 22:52:02 +0200

    removed some excess files
    removed some excess files.

--- LLL.py
+++
@@ -1,4 +0,0 @@
-my_string = "fgjan1239fnfj"
-
-numbers = 0
-letters = 0
--- dikc.py
+++
@@ -1,8 +0,0 @@
-# coding=utf8
-import os
-# os.system("fsutil file createnew background.jpg 10000")
-image = open(r'background.jpg', 'w', encoding="utf-8")
-data = open(r'black_jack.jpg', 'r', encoding="utf8", errors='ignore')
-
-data_to_write = data.read()
-image.write(data_to_write)
